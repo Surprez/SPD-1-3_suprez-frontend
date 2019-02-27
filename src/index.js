b0921e52a1f25d83836c448a5ab10b191d212daa
Nolan Kovacik <noltron000@gmail.com>
2019-02-27 14:21:48 -0800

    🏗react app ready for additions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './jsx/App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
