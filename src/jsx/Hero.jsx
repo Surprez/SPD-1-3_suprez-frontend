863bec5ff3d237546a358cf35b65e20cba7864cb
Nolan Kovacik <noltron000@gmail.com>
2019-03-02 15:33:27 -0800

    🎆 good start to front page

--- src/jsx/Hero.jsx
+++ src/jsx/Hero.jsx
@@ -16,19 +16,21 @@ const Hero = (props) => {
 	return (
 		<div className='hero'>
 			<div>
-				<img src={logo} className='logo' alt='logo' />
+				<div>
+					<img src={logo} className='logo' alt='logo' />
 
-				<header>
-					<h1 className='brand'>Suprez</h1>
-					<h2 className='subheading'>Not just presentations.<br /><em>Experiences.</em></h2>
-				</header>
+					<header>
+						<h1 className='brand'>Suprez</h1>
+						<h2 className='subheading'>Not just presentations.<br /><em>Experiences.</em></h2>
+					</header>
 
-			</div>
+				</div>
 
-			<div>
-				<h4>
-					<Link className='call-to-action button' to='/speak'>Begin Your Presentation</Link>
-				</h4>
+				<div>
+					<h4>
+						<Link className='call-to-action button' to='/speak'>Begin Your Presentation</Link>
+					</h4>
+				</div>
 			</div>
 
 			<div>
