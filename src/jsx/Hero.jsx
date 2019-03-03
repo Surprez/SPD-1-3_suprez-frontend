53e58c44c4222cfb5ab59814f65d3448ff2a477e
Nolan Kovacik <noltron000@gmail.com>
2019-03-02 22:03:05 -0800

    🎨 css updates

--- src/jsx/Hero.jsx
+++ src/jsx/Hero.jsx
@@ -1,7 +1,7 @@
 // React imports
 import React from 'react'
 import { Link } from 'react-router-dom';
-import logo from '../svg/logo.svg';
+// import logo from '../svg/logo.svg';
 
 // CSS files
 import '../css/hero.css';
