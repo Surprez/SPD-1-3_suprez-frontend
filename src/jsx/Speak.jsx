2e2967ddcaec04713732668accbe61918663db6e
Keoni Murray <keonimurray45@gmail.com>
2019-03-02 16:09:13 -0800

    speech to text is working

--- src/jsx/Speak.jsx
+++ src/jsx/Speak.jsx
@@ -1,30 +1,62 @@
 import React, { Component } from "react";
 import SpeechRecognition from "react-speech-recognition";
+// import { worker } from "cluster";
 
 class Dictaphone extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      transcript: null
+    };
+  }
+
+  // when you end listening,
+  // update state with final transcript
+  // pass this function to the element that does this worker, via props
+  //   }
+
+  // when you want to get the end of transcript
+  // update state with that transcipt setState({transcript: transcript})
+
   render() {
     const {
-      transcript,
+      //   transcript,
       resetTranscript,
       browserSupportsSpeechRecognition,
       startListening,
-      stopListening
+      stopListening,
+      finalTranscript
     } = this.props;
 
     if (!browserSupportsSpeechRecognition) {
       return null;
     }
+    console.log("the state is:", this.state.transcript);
 
     return (
       <div>
         <button onClick={startListening}>Start listening</button>
-        <button onClick={stopListening}>Stop Listening</button>
+        <button
+          onClick={() => {
+            // other code runs here
+            this.setState({ transcript: finalTranscript });
+
+            stopListening();
+
+            console.log("final transcript is:", finalTranscript);
+            console.log("the state is:", this.state.transcript);
+          }}
+        >
+          Stop Listening
+        </button>
+
         <button onClick={resetTranscript}>Reset</button>
-        <span>{transcript}</span>
       </div>
     );
   }
 }
+
 const options = {
   autoStart: false
 };
