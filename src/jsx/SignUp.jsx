cde64763a91386e0ef4cbe90c0ea82c069709026
Keoni Murray <keonimurray45@gmail.com>
2019-03-02 21:25:05 -0800

    save input to state

--- src/jsx/SignUp.jsx
+++ src/jsx/SignUp.jsx
@@ -1,32 +1,72 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class SignUp extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Sign Up</h2>
+  constructor(props) {
+    super(props);
 
-				<form action='/signup' method='POST'>
-					<div>
-						<label>Username</label><br />
-						<input type='text' id='username' name='username' required></input>
-					</div>
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
 
-					<div>
-						<label>Password</label><br />
-						<input type='password' id='password' name='password' required></input>
-					</div>
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
 
-					<div>
-						<button type='submit'>Sign Up</button>
-						<Link to='/'>return home</Link>
-					</div>
-				</form>
-			</div>
-		);
-	}
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+
+    console.log("handlechange", this.state.name, this.state.password);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("handle submit", this.state.username, this.state.password);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Sign Up</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Username</label>
+            <br />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <button type="submit">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SignUp
+export default SignUp;
