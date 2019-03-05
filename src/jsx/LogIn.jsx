0efbd681ec13da8e1295b2ab9546aa3f0820b90e
Nolan Kovacik <noltron000@gmail.com>
2019-03-05 10:47:57 -0800

    🔖 unifying spacing

--- src/jsx/LogIn.jsx
+++ src/jsx/LogIn.jsx
@@ -2,68 +2,68 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
-  handleInputChange = event => {
-    const { value, name } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-  onSubmit = event => {
-    event.preventDefault();
-    	return fetch('/login', {
-      method: "POST",
-      body: JSON.stringify(this.state),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(res => {
-        if (res.status === 200) {
-          this.props.history.push("/speak");
-        } else {
-          const error = new Error(res.error);
-          throw error;
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        alert("error logging in!");
-      });
-  };
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            required
-          />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: "",
+			password: ""
+		};
+	}
+	handleInputChange = event => {
+		const { value, name } = event.target;
+		this.setState({
+			[name]: value
+		});
+	};
+	onSubmit = event => {
+		event.preventDefault();
+		return fetch('/login', {
+			method: "POST",
+			body: JSON.stringify(this.state),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => {
+				if (res.status === 200) {
+					this.props.history.push("/speak");
+				} else {
+					const error = new Error(res.error);
+					throw error;
+				}
+			})
+			.catch(err => {
+				console.log(err);
+				alert("error logging in!");
+			});
+	};
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+					<input
+						type="text"
+						name="username"
+						placeholder="Enter username"
+						value={this.state.username}
+						onChange={this.handleInputChange}
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Enter password"
+						value={this.state.password}
+						onChange={this.handleInputChange}
+						required
+					/>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+	}
 }
 
 {
-  /* <Link to='/'>return home</Link> */
+	/* <Link to='/'>return home</Link> */
 }
--- src/jsx/SignUp.jsx
+++ src/jsx/SignUp.jsx
@@ -4,71 +4,71 @@ import React, { Component } from "react";
 //need this to send form data
 
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
+		this.state = {
+			username: "",
+			password: ""
+		};
+	}
 
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
+	validateForm() {
+		return this.state.email.length > 0 && this.state.password.length > 0;
+	}
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
 
-    console.log("handlechange", this.state.name, this.state.password);
-  };
+		console.log("handlechange", this.state.name, this.state.password);
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log("handle submit", this.state.username, this.state.password);
-  };
+	handleSubmit = event => {
+		event.preventDefault();
+		console.log("handle submit", this.state.username, this.state.password);
+	};
 
-  render() {
-    return (
-      <div>
-        <h2>Sign Up</h2>
+	render() {
+		return (
+			<div>
+				<h2>Sign Up</h2>
 
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Username</label>
-            <br />
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<label>Username</label>
+						<br />
+						<input
+							type="text"
+							id="username"
+							name="username"
+							value={this.state.username}
+							onChange={this.handleChange}
+							required
+						/>
+					</div>
 
-          <div>
-            <label>Password</label>
-            <br />
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
+					<div>
+						<label>Password</label>
+						<br />
+						<input
+							type="password"
+							id="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.handleChange}
+							required
+						/>
+					</div>
 
-          <div>
-            <button type="submit">Sign Up</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+					<div>
+						<button type="submit">Sign Up</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignUp;
