4c3e8d14fd2b127a9408a4522890a5e9d98ad5a6
Keoni Murray <keonimurray45@gmail.com>
2019-03-05 01:20:26 -0800

    login in progress

--- src/jsx/LogIn.jsx
+++ src/jsx/LogIn.jsx
@@ -1,32 +1,69 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// Login.jsx
+import React, { Component } from "react";
 
-
-class Login extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Log In</h2>
-
-				<form action='/login' method='POST'>
-					<div>
-						<label>Username</label><br />
-						<input type='text' id='username' name='username' required></input>
-					</div>
-
-					<div>
-						<label>Password</label><br />
-						<input type='password' id='password' name='password' required></input>
-					</div>
-
-					<div>
-						<button type='submit'>Log In</button>
-						<Link to='/'>return home</Link>
-					</div>
-				</form>
-			</div>
-		);
-	}
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  handleInputChange = event => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  onSubmit = event => {
+    event.preventDefault();
+    	return fetch('/login', {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          this.props.history.push("/");
+        } else {
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        alert("error logging in!");
+      });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            required
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login
+{
+  /* <Link to='/'>return home</Link> */
+}
