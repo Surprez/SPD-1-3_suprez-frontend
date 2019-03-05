c4c69af1eaba696f4092bfaa3586e456e51b05bc
Keoni Murray <keonimurray45@gmail.com>
2019-03-05 01:49:14 -0800

    login completed

--- src/jsx/LogIn.jsx
+++ src/jsx/LogIn.jsx
@@ -26,7 +26,7 @@ export default class Login extends Component {
     })
       .then(res => {
         if (res.status === 200) {
-          this.props.history.push("/");
+          this.props.history.push("/speak");
         } else {
           const error = new Error(res.error);
           throw error;
