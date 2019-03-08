e3f7b20b403e6abe361d4ea038463166ca197e07
Nolan Kovacik <noltron000@gmail.com>
2019-03-07 22:56:09 -0800

    💫 very stateful confusion with keywords

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,5 +1,5 @@
 // import logo from '../svg/logo.svg';
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Keywords from './Keywords.jsx'
 import Navbar from './Navbar.jsx';
@@ -12,19 +12,35 @@ import Home from './Home.jsx';
 import '../css/main.css';
 import '../css/fonts.css';
 
-const App = () => {
-	return (
-		<Router>
-			<div>
-				<Navbar />
-				<Route exact path='/' component={Home} />
-				<Route exact path='/login' component={Login} />
-				<Route exact path='/signup' component={SignUp} />
-				<Route exact path='/keywords' component={Keywords} />
-				<Route exact path='/speak' component={Speak} />
-			</div>
-		</Router>
-	);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			keywords: []
+		};
+	}
+
+	saveKeywords = (newKeywords) => {
+		console.log("HOORAY")
+		this.setState({ keywords: newKeywords })
+		console.log(this.state.keywords)
+	}
+
+	render() {
+		return (
+			<Router>
+				<div>
+					<Navbar />
+					<Route exact path='/' component={Home} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/signup' component={SignUp} />
+					<Route exact path='/keywords' component={Keywords} saveKeywords={this.saveKeywords.bind(this)} />
+					<Route exact path='/speak' component={Speak} keywords={this.state.keywords} />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
